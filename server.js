388ca8ffc3e9610419157947eb68250bd5bcf8f3
Rohit000 <rohit@crowdvolley.com>
2025-11-27 11:17:37 +0530

    response send

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-// const xmlparser = require("express-xml-bodyparser");
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -8,19 +7,11 @@ app.use(express.json());
 
 app.use(bodyParser.text({ type: "application/xml" }));
 
-app.post("/access-token", async (req, res) => {
-    console.log("Received body:", req.body);
-    return res.send("OK");
-});
-
-app.post("/user", async (req, res) => {
-    console.log("Received body:", req.body);
-    return res.send("OK");
-});
-
 app.post("/companies", async (req, res) => {
     console.log("Received body:", req.body);
-    return res.send("OK");
+
+    res.setHeader("Content-Type", "application/xml");
+    return res.send(req.body);
 });
 
-app.listen(3000, () => console.log("Running on http://localhost:3000"));
+app.listen(3000, () => console.log("Running on https://vartopia-api.onrender.com/"));
